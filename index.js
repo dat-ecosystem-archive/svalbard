f3fdf64fdb9727e3f35d10703bb393e4547909ee
Max Ogden <max@maxogden.com>
2016-12-13 13:30:22 -0800

    initial prototype

---
+++ index.js
@@ -0,0 +1,26 @@
+var archiver = require('hypercore-archiver')
+var archiverServer = require('archiver-server')
+var normcore = require('normcore')
+var ndjson = require('ndjson')
+
+module.exports = function (key, opts) {
+  var feed = normcore(key, opts)
+  var archives = archiver('./archive')
+  var datServer = archiverServer(archives, opts)
+  
+  feed.createReadStream().pipe(ndjson.parse())
+    .on('data', function (obj) {
+      if (obj.type === 'add') {
+        console.log('adding', obj.key)
+        archives.add(obj.key, function (err) {
+          if (err) console.error('error adding', err)
+        })
+      }
+      if (obj.type === 'remove') {
+        console.log('removing', obj.key)
+        archives.remove(obj.key, function (err) {
+          if (err) console.error('error renoving', err)
+        })
+      }
+    })
+}
