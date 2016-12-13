878c2870ef2d06846c50db350ec57ea7a26e6d3b
Max Ogden <max@maxogden.com>
2016-12-13 13:33:25 -0800

    make dat link configurable

--- cli.js
+++ cli.js
@@ -2,6 +2,6 @@
 
 var createNode = require('./index.js')
 
-var key = 'f69a90011f3619b890e6c950111bdba93118e9e1bc2ed3906ccbe13eb5e20dc8'
+var key = process.argv[2]
 
 createNode(key, {dht: false, dontShare: true})
